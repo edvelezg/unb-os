d935d3936ff4f0e599e0af491b38e6454f1373fa
EdVelez.G <EdVelez.G@ff7ef672-0eec-11df-b535-e1565790bc13>
2010-02-15 02:42:14 +0000

    spo1 and spo2 terminate but PerB() goes indefinitely

--- trunk/mydesign/main.c
+++ trunk/mydesign/main.c
@@ -12,7 +12,7 @@ void foo()
 void spo1()
 {
     int i;
-	serial_print ("I'm in Spo1()!\n");
+    serial_print ("I'm in Spo1()!\n");
     for ( i = 1; i != 0 ; ++i );
     for ( i = 1; i != 0 ; ++i );
     for ( i = 1; i != 0 ; ++i );
@@ -25,13 +25,12 @@ void spo1()
     for ( i = 1; i != 0 ; ++i );
     for ( i = 1; i != 0 ; ++i );
     for ( i = 1; i != 0 ; ++i );
-//  OS_Yield();
 }
 
 void spo2()
 {
     int i;
-	serial_print ("I'm in Spo2()!\n");
+    serial_print ("I'm in Spo2()!\n");
     for ( i = 1; i != 0 ; ++i );
     for ( i = 1; i != 0 ; ++i );
     for ( i = 1; i != 0 ; ++i );
@@ -44,78 +43,97 @@ void spo2()
     for ( i = 1; i != 0 ; ++i );
     for ( i = 1; i != 0 ; ++i );
     for ( i = 1; i != 0 ; ++i );
-	//OS_Yield();
 }
 
-void spo3()
+void per1()
 {
     int i;
-	serial_print ("I'm in PerA()!\n");
-    for ( i = 1; i != 0 ; ++i );
-    for ( i = 1; i != 0 ; ++i );
-    for ( i = 1; i != 0 ; ++i );
-    for ( i = 1; i != 0 ; ++i );
-    for ( i = 1; i != 0 ; ++i );
-    for ( i = 1; i != 0 ; ++i );
-    for ( i = 1; i != 0 ; ++i );
-    for ( i = 1; i != 0 ; ++i );
-    for ( i = 1; i != 0 ; ++i );
-    for ( i = 1; i != 0 ; ++i );
-    for ( i = 1; i != 0 ; ++i );
-    for ( i = 1; i != 0 ; ++i );
-	//OS_Yield();
+
+    while ( 1 )
+    {
+        serial_print ("I'm in PerA() Part 1!\n");
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        OS_Yield();
+        serial_print ("I'm in PerA Part 2()!\n");
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+    }
 }
 
-void spo4()
+void per2()
 {
-    int i;
-	serial_print ("I'm in PerB()!\n");
-    for ( i = 1; i != 0 ; ++i );
-    for ( i = 1; i != 0 ; ++i );
-    for ( i = 1; i != 0 ; ++i );
-    for ( i = 1; i != 0 ; ++i );
-    for ( i = 1; i != 0 ; ++i );
-    for ( i = 1; i != 0 ; ++i );
-    for ( i = 1; i != 0 ; ++i );
-    for ( i = 1; i != 0 ; ++i );
-    for ( i = 1; i != 0 ; ++i );
-    for ( i = 1; i != 0 ; ++i );
-    for ( i = 1; i != 0 ; ++i );
-    for ( i = 1; i != 0 ; ++i );
-	//OS_Yield();
+    while ( 1 )
+    {
+        int i;
+        serial_print ("I'm in PerB()!\n");
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        for ( i = 1; i != 0 ; ++i );
+        OS_Yield();
+    }
 }
 
 /* main() can then create processes and initialize the PPP[] and PPPMax[] arrays */
 
 static inline void serial_send (char c)
 {
-  /* Wait until the SIO has finished to send the character.  */
-  while (!(_io_ports[M6811_SCSR] & M6811_TDRE))
-    continue;
+    /* Wait until the SIO has finished to send the character.  */
+    while ( !(_io_ports[M6811_SCSR] & M6811_TDRE) )
+        continue;
 
-  _io_ports[M6811_SCDR] = c;
-  _io_ports[M6811_SCCR2] |= M6811_TE;
+    _io_ports[M6811_SCDR] = c;
+    _io_ports[M6811_SCCR2] |= M6811_TE;
 }
 
 void serial_print (char *msg)
 {
-  while (*msg != 0)
-    serial_send (*msg++);
+    while ( *msg != 0 )
+        serial_send (*msg++);
 }
 
 
 int main (int argc, char *argv[])
 {
     int my_var, my_int;
-	
-	/* Configure the SCI to send at M6811_DEF_BAUD baud.  */
-	  _io_ports[M6811_BAUD] = M6811_DEF_BAUD;
 
-	  /* Setup character format 1 start, 8-bits, 1 stop.  */
-	  _io_ports[M6811_SCCR1] = 0;
+    /* Configure the SCI to send at M6811_DEF_BAUD baud.  */
+    _io_ports[M6811_BAUD] = M6811_DEF_BAUD;
 
-	  /* Enable receiver and transmitter.  */
-	  _io_ports[M6811_SCCR2] = M6811_TE | M6811_RE;
+    /* Setup character format 1 start, 8-bits, 1 stop.  */
+    _io_ports[M6811_SCCR1] = 0;
+
+    /* Enable receiver and transmitter.  */
+    _io_ports[M6811_SCCR2] = M6811_TE | M6811_RE;
 
     /**
      *   The PPP[] array is a sequence of names which specifies the execution
@@ -124,23 +142,23 @@ int main (int argc, char *argv[])
      *   than once.
      */
 //  OS_DI();
-	OS_Init();
-	OS_Create(spo1, 0, SPORADIC, 1);
-	OS_Create(spo2, 0, SPORADIC, 1);
-	OS_Create(spo3, 0, PERIODIC, 'A');
-	OS_Create(spo4, 0, PERIODIC, 'B');
+    OS_Init();
+    OS_Create(spo1, 0, SPORADIC, 1);
+    OS_Create(spo2, 0, SPORADIC, 1);
+    OS_Create(per1, 0, PERIODIC, 'A');
+    OS_Create(per2, 0, PERIODIC, 'B');
 
-	PPP[0] = 'A';
-	PPP[1] = IDLE;
-	PPP[2] = 'B';
-	PPP[3] = IDLE;
-	PPPMax[0] = 2;
-	PPPMax[1] = 3;
-	PPPMax[2] = 4;
-	PPPMax[3] = 5;
-	PPPLen = 4;
+    PPP[0] = IDLE;
+    PPP[1] = IDLE;
+    PPP[2] = 'B';
+    PPP[3] = 'A';
+    PPPMax[0] = 2;
+    PPPMax[1] = 3;
+    PPPMax[2] = 4;
+    PPPMax[3] = 5;
+    PPPLen = 4;
 
-	OS_Start();
-	
+    OS_Start();
+
     return 0;
 }
