21d06926e149aeb5706b555f259ec23aea8c3889
EdVelez.G <EdVelez.G@ff7ef672-0eec-11df-b535-e1565790bc13>
2010-02-16 20:17:30 +0000

    CP 5, error when I add the semaphores

---
+++ mydesign/ReadMe.txt
@@ -0,0 +1,10 @@
+To compile the Operating system:
+Run the build.bat file. Make sure you have the m6811 tools installed or if installed
+in path other than /usr/bin, change accordingly.
+To Test the Operating Sytem:
+Run the test.bat file
+
+Basic Tests:
+Run with no sporadic processes
+Run with no periodic processes
+Run with device process.
--- mydesign/fifo.h
+++ mydesign/fifo.h
@@ -16,4 +16,59 @@ typedef struct fifo_struct
 fifo_struct arrFifos[MAXFIFO];
 static int numFifos = 0; /* keeps track of the number of fifos */
 
+FIFO OS_InitFiFo()
+{
+    /* gets next available message queue */
+    if ( numFifos < MAXFIFO )
+    {
+        arrFifos[numFifos].fillCount = 0;
+        arrFifos[numFifos].next = 0;
+        arrFifos[numFifos].first = 0;
+        ++numFifos;
+        return numFifos;
+    }
+    else
+    {
+        /* ran out of message queues */
+        return INVALIDFIFO;
+    }
+}
+
+void OS_Write(FIFO f, int val)
+{
+    int idx = f - 1;
+    fifo_struct *curFifo = &arrFifos[idx];
+
+    /* the buffer still has space to write
+       if it is full writes are ignored */
+    if ( curFifo->fillCount < FIFOSIZE )
+    {
+        curFifo->buffer[curFifo->next] = val;
+        curFifo->next = (curFifo->next + 1) % FIFOSIZE;
+    }
+
+    /* increment fillcount if not full */
+    curFifo->fillCount = (curFifo->fillCount == FIFOSIZE) ? FIFOSIZE : ++curFifo->fillCount;
+}
+
+BOOL OS_Read(FIFO f, int *val)
+{
+    int idx = f - 1;
+    fifo_struct *curFifo = &arrFifos[idx];
+
+    /* the buffer is empty */
+    if ( curFifo->fillCount <= 0 )
+    {
+        return FALSE;
+    }
+    /* there are still elements in the buffer */
+    else
+    {
+        *val = curFifo->buffer[curFifo->first];
+        curFifo->first = (curFifo->first + 1) % FIFOSIZE;
+        curFifo->fillCount--;
+        return TRUE;
+    }
+}
+
 #endif /* _FIFO_H_ */
