4d9e0ef3b4735861dfa05035484f6ca531af1353
EdVelez.G <EdVelez.G@ff7ef672-0eec-11df-b535-e1565790bc13>
2010-03-26 12:46:58 +0000

    fixed build.bat

--- trunk/mydesign/build.bat
+++ trunk/mydesign/build.bat
@@ -1,4 +1,4 @@
-rem /usr/bin/m6811-elf-gcc -g -Os -mshort -msoft-reg-count=0 -Wl,-m,m68hc11elfb -o main.elf main.c os.c
+rem /usr/bin/m6811-elf-gcc -g -Os -mshort -msoft-reg-count=0 -Wl,-m,m68hc11elfb -o main.elf main.c os.c lcd.c
 rem /usr/bin/m6811-elf-objcopy --only-section=.text --only-section=.rodata --only-section=.vectors --only-section=.data --output-target=srec main.elf main.s19
 
 echo off
@@ -6,6 +6,6 @@ cls
 IF EXIST main.elf erase main.elf
 echo === Compiling ===
 rem c:\usr\bin\m6811-elf-gcc -g -Os -mshort -Wl,-m,m68hc11elfb -o os.elf *.c
-/usr/bin/m6811-elf-gcc -g -Os -mshort -msoft-reg-count=0 -Wl,-m,m68hc11elfb -o main.elf main.c os.c
+/usr/bin/m6811-elf-gcc -g -Os -mshort -msoft-reg-count=0 -Wl,-m,m68hc11elfb -o main.elf main.c os.c lcd.c
 rem call run.bat
 /usr/bin/m6811-elf-objcopy --only-section=.text --only-section=.rodata --only-section=.vectors --only-section=.data --output-target=srec main.elf main.s19
