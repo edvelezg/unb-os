f9b0456a71c793967b7f6bba8be9786e193b134f
EdVelez.G <EdVelez.G@ff7ef672-0eec-11df-b535-e1565790bc13>
2010-04-09 03:00:38 +0000

    Work on semaphores only

---
+++ branches/mydesign-sems/build.bat
@@ -0,0 +1,2 @@
+/usr/bin/m6811-elf-gcc -g -Os -mshort -msoft-reg-count=0 -Wl,-m,m68hc11elfb -o main.elf main.c os.c
+/usr/bin/m6811-elf-objcopy --only-section=.text --only-section=.rodata --only-section=.vectors --only-section=.data --output-target=srec main.elf main.s19
---
+++ branches/mydesign-sems/process.h
@@ -0,0 +1,47 @@
+#ifndef _PROCESS_H_
+#define _PROCESS_H_
+
+typedef enum 
+{
+    NEW, RUNNING, WAITING, READY, TERMINATED
+} 
+States;
+
+/* process control block */
+typedef struct pcb_struct 
+{
+    PID                       pid;
+    int                       level;
+    States                    state;
+    int                       argument; /* argument */
+    int                       *sp; /* stack pointer */
+    void                      (*pc)(void); // function pointer
+    int                       frequency;
+    int                       countDown;
+    int                       name;
+    int                       sem_hold; // semaphore this holds
+    int                       sem_sleep; // semaphore it is sleeping on
+} ProcCtrlBlock;
+
+/* queue control block */
+typedef struct queue_struct
+{
+    ProcCtrlBlock*  procRefs[MAXPROCESS];  /* message circular buffer */
+    int             fillCount;         /* keeps track of the number of items */          
+    int             next;              /* next spot to write in */
+    int             first;             /* first element to read */
+
+} ProcQueue;
+
+/* software interrupt */
+void SWI(void);
+/* enqueue definition */
+void Enqueue(ProcQueue* prq, ProcCtrlBlock* p);
+/* dequeue definition */
+BOOL Dequeue(ProcQueue *prq, ProcCtrlBlock** p);
+void InitQueues();
+void Schedule(void);
+/* __attribute__ ((interrupt)) */
+void context_switch (void);
+
+#endif /* _PROCESS_H_ */
