1b8bf11b356da4926f63af65eb86123ab0145998
EdVelez.G <EdVelez.G@ff7ef672-0eec-11df-b535-e1565790bc13>
2010-04-10 04:42:55 +0000

    Added other device processes

--- trunk/mydesign/main.c
+++ trunk/mydesign/main.c
@@ -75,61 +75,29 @@ void producer1()
 
 void per1()
 {
-    while ( TRUE )
-    {
-		char i;
-        //sys_print_lcd("AAAAAA\n");
-		B_SET(_io_ports[M6811_PORTA], 3);
-		for ( i = 1 ; i != 0; ++i );
-		B_UNSET(_io_ports[M6811_PORTA], 3);
-		for ( i = 1 ; i != 0; ++i );
-//      OS_Terminate();
-    }
-}
-
-void per2()
-{
-    while ( TRUE )
-    {
-        sys_print_lcd("BBBBBB\n");
-//      OS_Terminate();
-    }
-}
-
-void dev1()
-{
     int i;
     unsigned int a = 0;
     unsigned int b = 0;
-    char *str = "   \n";
+    char *str = "    \0";
     while ( TRUE )
     {
-        serial_print ("I'm a device process\n");
-        serial_print ("That counts\n");
         b = a;
         str[2] = (b % 10) + '0';
         b /= 10;
         str[1] = (b % 10) + '0';
         b /= 10;
         str[0] = (b % 10) + '0';
-        serial_print (str);
+        sys_print_lcd(str);
         ++a;
-        for ( i = 1 ; i != 0; ++i );
-        for ( i = 1 ; i != 0; ++i );
-        for ( i = 1 ; i != 0; ++i );
-        for ( i = 1 ; i != 0; ++i );
-        for ( i = 1 ; i != 0; ++i );
-        for ( i = 1 ; i != 0; ++i );
-        OS_Yield(); /* must yield or terminate, if not it won't be interrupted.*/
     }
     OS_Terminate();
 }
 
-void dev2()
+void per2()
 {
     static int j = 0;
     unsigned int *myP;
-    char *str = "      ";
+    char *str = "       \0";
     unsigned int i;
 
     myP = (unsigned int *)&_io_ports[TCNT];
@@ -146,16 +114,8 @@ void dev2()
         str[2] = (i % 10) + '0';
         i /= 10;
         str[1] = (i % 10) + '0';
-        serial_print ("TCNTs value is:\n");
-        serial_print (str);
-        serial_print ("\n");
-        for ( i = 1; i != 0 ; ++i );
-        for ( i = 1; i != 0 ; ++i );
-        for ( i = 1; i != 0 ; ++i );
-        for ( i = 1; i != 0 ; ++i );
-        for ( i = 1; i != 0 ; ++i );
+        sys_print_lcd (str);
         ++j;
-        OS_Yield();  /* must yield or terminate, if not it won't be interrupted.*/
     }
     OS_Terminate();
 }
@@ -216,6 +176,82 @@ void consumer()
         OS_Yield();
     }
 //  OS_Terminate();
+}
+
+void dev3()
+{
+	char i;
+    while ( TRUE )
+    {
+		_io_ports[M6811_DDRD] = 0xFF; /* it sets the data direction to output for port d for all the pins*/
+		B_SET(_io_ports[M6811_PORTD],4); /* it sets the data direction to output for port d for all the pins*/
+		B_SET(_io_ports[M6811_PORTD],5); /* it sets the data direction to output for port d for all the pins*/
+		//_io_ports[M6811_PORTD] = 0xFF; /* I want 4 and 5 */
+		
+		/* Turns on right and left motors */
+		//B_SET(_io_ports[M6811_PORTA], 5);  // right
+		//B_SET(_io_ports[M6811_PORTA], 6);	// left
+		_io_ports[M6811_PORTA] = 0xFF; /* I want 4 and 5 */
+
+		for ( i = 1 ; i != 0; ++i );
+		for ( i = 1 ; i != 0; ++i );
+		for ( i = 1 ; i != 0; ++i );
+		//for ( i = 1 ; i != 0; ++i );
+		//for ( i = 1 ; i != 0; ++i );
+
+		B_UNSET(_io_ports[M6811_PORTA], 5);
+		B_UNSET(_io_ports[M6811_PORTA], 6);
+		OS_Yield();
+	}
+}
+
+void dev2()
+{
+    while ( TRUE )
+    {
+		int j;
+		char i;
+        //sys_print_lcd("AAAAAA\n");
+		for (j = 0; j < 255; ++j)
+		{
+			B_SET(_io_ports[M6811_PORTA], 3);
+			for ( i = 1 ; i != 0; ++i );
+			for ( i = 1 ; i != 0; ++i );
+			B_UNSET(_io_ports[M6811_PORTA], 3);
+			for ( i = 1 ; i != 0; ++i );
+			for ( i = 1 ; i != 0; ++i );
+		}
+		OS_Yield();
+    }
+}
+
+void dev1()
+{
+	char *a = "--------";
+	int i;
+	int lightValue;
+	while (TRUE)
+	{
+		B_SET(_io_ports[OPTION], 7); // switch port e to analog mode
+		OS_Yield();
+	
+		_io_ports[ADCTL] = 0; // right photocell is pin 0, so you set it to 0.
+
+		while (!(_io_ports[ADCTL] & 128)) // waits bit 7 to light up
+		{	
+		}
+
+		lightValue = _io_ports[ADR1];
+		a[2] = lightValue%10 + '0';
+		lightValue /= 10;
+		a[1] = (lightValue)%10 + '0';
+		lightValue /= 10;
+		a[0] = (lightValue)%10 + '0';
+
+		sys_print_lcd(a);
+		//OS_Yield();
+	}
+
 }
 
 static inline void serial_send (char c)
